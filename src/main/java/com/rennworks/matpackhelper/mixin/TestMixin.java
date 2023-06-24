cf9461eda7b2287e60652904bcd791438c363425
IMadeThisJustToPostThis <matzielbiz@gmail.com>
2023-06-24 22:39:56 +0200

    added test mixin

---
+++ src/main/java/com/rennworks/matpackhelper/mixin/TestMixin.java
@@ -0,0 +1,12 @@
+package com.rennworks.matpackhelper.mixin;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+@Mixin(Items.class)
+public abstract class TestMixin implements IEventBus {
+
+}
